10e68ad67169d6a40efcedd2d5b1a0c8189e52cf
djb77 <dwayne.bakewell@gmail.com>
2018-01-20 16:43:09 +1100

    fs: Update F2FS from @jaegeuk f2fs-stable v4.14-rc1-3.18
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- fs/sync.c
+++ fs/sync.c
@@ -204,6 +204,7 @@ static int async_fsync(struct file *file)
  */
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
+	struct inode *inode = file->f_mapping->host;
 	int err;
 	ktime_t fsync_t, fsync_diff;
 	char pathname[256], *path;
@@ -218,6 +219,13 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 
 	if (async_fsync(file) && !bypass(path))
 		return 0;
+
+	if (!datasync && (inode->i_state & I_DIRTY_TIME)) {
+		spin_lock(&inode->i_lock);
+		inode->i_state &= ~I_DIRTY_TIME;
+		spin_unlock(&inode->i_lock);
+		mark_inode_dirty_sync(inode);
+	}
 
 	trace_vfs_fsync(file);
 	err = file->f_op->fsync(file, start, end, datasync);
